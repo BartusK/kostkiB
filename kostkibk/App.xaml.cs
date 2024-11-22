90453c9e0bcfc9ddb022e99e2e1f074bccd2aa51
bartu <bartu@BRT-PC>
2024-11-22 21:01:51 +0100

    Dodaj pliki projektów.

---
+++ kostkibk/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace kostkibk
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ kostkibk/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace kostkibk
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
